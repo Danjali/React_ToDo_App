a481c84f5dd133bf24c5f3a3bdd06485d8885ef2
Danjali <anjalideoasarkar@gmail.com>
2019-02-20 13:13:34 +0530

    adds styling

--- src/itemList.js
+++ src/itemList.js
@@ -17,25 +17,32 @@ export default class ItemList extends React.Component {
   render() {
     let items  = this.props.listItems;
     return (
-      <ul className="displayList">
-        {
-          items.map((item, index) => {
-            return (
-              <li key={index} className="list">
-                <span className="listText" title={item.text}>{item.text}</span>
-                <span className="taskState">{item.taskState}</span>
-                <div className="buttons">
-                  <button className="actionButton" onClick={this.editItem.bind(this, index)}>
+      <div>
+        { items.length > 0 &&
+          <div className="Table">
+            <div className="CellHeadings">Description</div>
+            <div className="CellHeadings">Status</div>
+            <div className="CellHeadings">Actions</div>
+            {
+              items.map((item, index) => {
+                return (
+                  <div key={index} className="Row">
+                    <div className="Cell" title={item.text}>{item.text}</div>
+                    <div className="Cell">{item.taskState}</div>
+                    <div className="Cell">
+                      <button className="actionButton" onClick={this.editItem.bind(this, index)}>
                     Edit</button>
-                  <button className="actionButton" onClick={this.markCompletedItem.bind(this, index)}>
+                      <button className="actionButton" onClick={this.markCompletedItem.bind(this, index)}>
                     Mark as Complete</button>
-                  <button className="actionButton" onClick={this.deleteItem.bind(this, index)}>Delete</button>
-                </div>
-              </li>
-            )
-          })
+                      <button className="actionButton" onClick={this.deleteItem.bind(this, index)}>Delete</button>
+                    </div>
+                  </div>
+                );
+              })
+            }
+          </div>
         }
-      </ul>
-    )
+      </div>
+    );
   }
 }
--- src/todoList.js
+++ src/todoList.js
@@ -7,11 +7,10 @@ export default class TodoList extends React.Component {
     super();
     this.state = {
       items: [],
-      searchedItems:[]
+      searchedItems:[],
+      taskState: 'To Do',
+      buttonType: 'Add'
     };
-    this.taskState = "To Do";
-    this.buttonType = "Add";
-    this.msg = "";
     this.addItem = this.addItem.bind(this);
     this.deleteItem = this.deleteItem.bind(this);
     this.searchItem = this.searchItem.bind(this);
@@ -21,71 +20,63 @@ export default class TodoList extends React.Component {
 
   addItem() {
     let inputText = this.refs.taskInput.value;
-    if(this.buttonType === "Update") {
+    if (this.state.buttonType === 'Update') {
       this.state.items[this.editItemId].text = inputText;
-      this.buttonType = "Add";
-      this.refs.taskInput.value = '';
+    } else if(inputText) {
+      this.state.items = [...this.state.items, {text: inputText, taskState: this.state.taskState}];
     }
-    else if (inputText) {
-      this.state.items = [...this.state.items, {text: inputText, taskState: this.taskState}];
-      this.refs.taskInput.value = '';
-    }
-    else {
-      this.msg = "Empty task cannot be added";
-    }
+
+    this.refs.taskInput.value = '';
     this.setState({
-        items: this.state.items
+      searchedItems: this.state.items,
+      buttonType: 'Add'
     });
   }
 
   deleteItem(id) {
     this.setState({
-         items: this.state.items.filter((items,key) => key !== id)
-    })
+      searchedItems: [...this.state.searchedItems].filter((items,key) => key !== id)
+    });
   }
 
   markCompletedItem(id) {
-    this.state.items[id].taskState = "Completed";
+    this.state.items[id].taskState = 'Completed';
     this.setState({
-        items: this.state.items
-    })
+      searchedItems: this.state.items
+    });
   }
 
   editItem(id) {
     this.refs.taskInput.value = this.state.items[id].text;
-    this.buttonType = "Update";
     this.editItemId = id;
     this.setState({
-        items: this.state.items
-    })
+      searchedItems: this.state.items,
+      buttonType: 'Update'
+    });
   }
 
-  searchItem(searchedText) {
-    const result = this.state.items.filter(item => {
-         return item.text.toLowerCase().includes(searchedText.toLowerCase());
-    });
+  searchItem(searchValue) {
+    const result = [...this.state.items].filter(item => item.text.search(searchValue.toLowerCase()) > -1);
     this.setState({
       searchedItems: result
-    })
+    });
   }
 
   render() {
-    let { items } = this.state;
+    let { items,searchedItems,buttonType} = this.state;
     return (
       <div className="list">
         <h1>ToDo List</h1>
         <div>
-          <input className="input" ref="taskInput" type="text" placeholder="Enter Task"/>
-          <button onClick={this.addItem}>{this.buttonType}</button>
+          <input className="input" ref="taskInput" size="40" type="text" placeholder="Enter Task"/>
+          <button onClick={this.addItem}>{buttonType}</button>
+          {items.length>0 &&
+            <ItemSearch searchItem={this.searchItem}/>
+          }
         </div>
         <div>
-          <ItemList listItems={items} deleteItem={this.deleteItem} markCompletedItem={this.markCompletedItem} editItem={this.editItem}/>
+          <ItemList listItems={searchedItems} deleteItem={this.deleteItem} markCompletedItem={this.markCompletedItem} editItem={this.editItem}/>
         </div>
-        {items &&
-        <div>
-          <ItemSearch searchedListItems={this.state.searchedItems} searchItem={this.searchItem}/>
-        </div>
-        }
       </div>
     );
   }
